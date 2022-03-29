ea4165ec395be3dfcafe0bae18a0600749abe18a
Nitin57955795 <101919051+Nitin57955795@users.noreply.github.com>
2022-03-29 17:54:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+ 
+var track;
+
+
+function preload(){
+  //pre-load images
+
+  track = loadImage("path.png")
+}
+
+//home and also restart by using ___ script .crc HTML p5.dom.min.js
+// script src"p5.play node s on and then minecraft is my favourite and time pass game 
+// all name of 
+// ohh no now we use velocity Y and then we write copywrite movie is being uploading in power ponit
+// function image () === obstacle 
+// we use some software to use and make something original 
+// some are power point and ms ecexl and paint
+// we use notepad to write HTMl 
+// and thank you shri ram 
+
+
+
+
+function setup(){
+  createCanvas(600,600);
+  //create sprites here
+  track = createSprite(200,200);
+  track.addImage("path.png",track);
+}
+
+function draw() {
+  background(0);
+
+}
